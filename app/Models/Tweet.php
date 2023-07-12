343b05698d3e3132ea2636e902642abbd2d9eb76
prepad <prepadobnii@gmail.com>
2023-07-12 21:59:31 +0700

    some things for db and seeding

---
+++ app/Models/Tweet.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tweet extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tweets';
+
+    protected $guarded = [
+        'id'
+    ];
+
+    protected $fillable = [
+        'text',
+        'theme',
+        'created_at',
+        'updated_at',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+    ];
+}
---
+++ database/factories/TweetFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tweet>
+ */
+class TweetFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'text' => fake()->text(),
+            'theme' => fake()->word(),
+            'user_id' => User::inRandomOrder()->first()->id,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
